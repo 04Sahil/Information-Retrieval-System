c1ddf9a385b8a01eae63c434dbd1882ba8941771
04Sahil <sahilkale362@gmail.com>
2025-07-25 15:17:33 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Sahil',
+    author_email= 'Information@gmail.ai',
+    packages= find_packages(),
+    install_requires = []
+
+)
